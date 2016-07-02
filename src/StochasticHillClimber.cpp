af4ac4ab79d73d85c1bce16b3bce8726a925ca34
Tobias Hänel <contact@tobias-haenel.de>
2016-07-02 23:17:12 +0200

    Improved best solution checks in SHC

--- src/StochasticHillClimber.cpp
+++ src/StochasticHillClimber.cpp
@@ -11,16 +11,16 @@ std::shared_ptr<Schedule> StochasticHillClimber::findSolution(double time_limit)
     while (not isTimeLimitReached(time_limit))
     {
         auto comp_sched = generateNeighbourSolution(*curr_sched);
+        if (comp_sched->exec_time() < best_sched->exec_time())
+        {
+            best_sched = comp_sched;
+        }
 
         auto accept_new_prob = 1.0 / (1.0 + exp((static_cast<int>(comp_sched->exec_time()) - static_cast<int>(curr_sched->exec_time())) / T));
 
         if (std::generate_canonical<double, std::numeric_limits<double>::digits>(current_random_engine()) <= accept_new_prob)
         {
             curr_sched = comp_sched;
-            if (curr_sched->exec_time() < best_sched->exec_time())
-            {
-                best_sched = curr_sched;
-            }
         }
     }
     return best_sched;
